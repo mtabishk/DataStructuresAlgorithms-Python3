b30df8f343ac71e064ebe9acbfc29bddce716b1c
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2021-01-13 20:24:36 +0530

    factorial recursion

---
+++ factorial_iterative.py
@@ -0,0 +1,14 @@
+# Factorial of a number
+# Time Complexity: 
+def factorial_iter(num):
+    if num == 0 or num == 1: return 1
+    fac = 1
+    while num > 1:
+        fac = fac * num
+        num = num - 1
+
+    return fac
+
+n = int(input("Enter the number: "))
+print(factorial_iter(n))
+
---
+++ factorial_recursive.py
@@ -0,0 +1,9 @@
+# Factorial of a number recursively
+# Time Complexity: O(n)
+def factorial_recv(num):
+    # base case
+    if  num == 1: return 1
+    return num * factorial_recv(num-1)
+
+n = int(input("Enter the number: "))
+print(factorial_recv(n))

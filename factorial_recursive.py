c46a6b103fd003c0c9a5c901947dd199692572ea
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2021-01-19 19:39:31 +0530

    update

--- factorial_recursive.py
+++ factorial_recursive.py
@@ -2,7 +2,7 @@
 # Time Complexity: O(n)
 def factorial_recv(num):
     # base case
-    if  num == 1: return 1
+    if  num == 1 or num == 0: return 1
     return num * factorial_recv(num-1)
 
 n = int(input("Enter the number: "))

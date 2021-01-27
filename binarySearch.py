a34711a6e83645ec4235377757c9238a0ec669a6
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2021-01-27 20:05:52 +0530

    update

---
+++ binarySearch.py
@@ -0,0 +1,22 @@
+# Binary Search Algorithm
+# Time Complexity: O(logn)
+def binary_search(arr, item):
+    start = 0
+    end = len(arr) - 1
+
+    while start <= end:
+        middle = middle = (start + end) // 2
+
+        if item == arr[middle]:
+            print(f"Item {item} found at index {middle}")
+            return
+        elif item < arr[middle]:
+            end = middle - 1
+        elif item > arr[middle]:
+            start = middle + 1
+    else: 
+        print(f"Item {item} not found")
+
+
+db = list(range(1,99999999))
+binary_search(db, 99999998)
--- linearSearch.py
+++ linearSearch.py
@@ -6,12 +6,14 @@ def sequential_search(arr, item):
     while pos < len(arr) and not found:
         if arr[pos] == item:
             found = True
-            print("Element found at position: {}".format(pos))
+            print(f"Element found at position: {pos}")
             break
         else:
             pos+=1
     return found
         
-a = [10,20,6,-11,22,45,76]
-element = int(input("Enter the element: "))
-print(sequential_search(a, element))
+#a = [10,20,6,-11,22,45,76]
+#element = int(input("Enter the element: "))
+#print(sequential_search(a, element))
+a = list(range(1,99999999))
+sequential_search(a,99999998)

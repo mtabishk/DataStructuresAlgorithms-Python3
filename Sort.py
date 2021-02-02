e1ec661083a8bb8e15acc76565e5abc69804d28d
Tabish <mtabishkhanday@gmail.com>
2021-02-02 20:17:44 +0530

    updated

--- Sort.py
+++ Sort.py
@@ -1,52 +1,55 @@
 class Sort:
-    # constructor
+    # constructor/ initializer
     def __init__(self):
         print(self)
 
     @staticmethod
-    def bubbleSort(arr):
+    def bubbleSort(data):
         swaps = 0
         # Size of Array
-        size = len(arr)
+        size = len(data)
         i = 0
         while i < size-1:
             j = 0
             while j < size-i-1:
-                if arr[j] > arr[j+1]:
+                if data[j] > data[j+1]:
                     # swap two elements
-                    arr[j], arr[j+1] = arr[j+1], arr[j]
+                    data[j], data[j+1] = data[j+1], data[j]
                     swaps = swaps + 1
                 j=j+1
             i=i+1
         print(f"Steps: {i} \t Swaps: {swaps}")
-        return arr
+        return data
 
     @staticmethod
-    def selectionSort(arr):
+    def selectionSort(data):
         # Size of Array
-        size = len(arr)
+        size = len(data)
         swaps = 0
 
         i = 0
         while i < size-1:
+            currentMinIndex = i
             j = i + 1
             while j <= size-1:
-                if arr[i] > arr[j]:
-                    # swap
-                    arr[i], arr[j] = arr[j], arr[i]
-                    swaps = swaps + 1
-
+                if data[j] < data[currentMinIndex]:
+                    currentMinIndex = j
                 j = j + 1
+            # swap
+            if i != currentMinIndex:
+                data[i], data[currentMinIndex] = data[currentMinIndex], data[i]
+                swaps = swaps + 1
             i = i + 1
-        print(f"Steps: {i} \t Swaps: {swaps}")
-        return arr
+        print(f"\nSteps: {i} \t Swaps: {swaps}")
+        return data
 
     def myfunc():
         print(f"Hello!")
 
 
-a = [2,6,9,4,8,1,5,10]
+a = [10,9,8,7,6,5,4,3,2,1,0]
 print(f"Unsorted Array is: {a}")
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
 # a.copy() makes a copy of list a and then passes to the function. Otherwise functions overwrites the same list.
 print(f"Sorted Array using Bubble Sort is: {Sort.bubbleSort(a.copy())}")
 print(f"Sorted Array using Selection Sort is: {Sort.selectionSort(a.copy())}")
